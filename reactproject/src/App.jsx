db7e472fb0156b3bffeede6316e6948c793e255a
irakeshbind <rakeshbind351998@gmail.com>
2024-07-10 23:07:44 +0530

    Add in the title component

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Component/Navbar/Navbar'
 import Hero from './Component/Hero/Hero'
 import Programs from './Component/Programs/Programs'
+import Title from './Component/Title/Title'
 const App = () => {
   return (
     <>
         <Navbar/>
         <Hero/>
         <div className='container'>
+        <Title/>
         <Programs/>
         </div>
     </>
---
+++ reactproject/src/Component/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Title.css'
+const Title = () => {
+  return (
+    <div className='title'>
+        <p>Our programg</p>
+        <h2>What We Program</h2>
+    </div>
+  )
+}
+
+export default Title
